88238cd24afc5c54d28cab978622d5a28b14081b
JordyAraujo <jordyaraujo@outlook.com>
2019-04-17 01:51:12 -0300

    Under Construction Page

---
+++ precache-manifest.c203f022bab8eed779e67a04c596ede9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e8a94adaeb601c0cdf4b864de59eff5",
+    "url": "/static/media/effort.9e8a94ad.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ccd4ac2bfa860b5d4d4c",
+    "url": "/static/js/main.f895c6f4.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ccd4ac2bfa860b5d4d4c",
+    "url": "/static/css/main.7c000360.chunk.css"
+  },
+  {
+    "revision": "3d9aff82e6377243d64252d09fa946f1",
+    "url": "/index.html"
+  }
+];
